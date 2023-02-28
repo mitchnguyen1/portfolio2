780829283ceca9b2dc39a003228f21429ef32820
mitch <mitch01nguyen@gmail.com>
2023-02-27 19:14:29 -0600

    adds more to about section

--- gsap.js
+++ gsap.js
@@ -5,7 +5,7 @@ let about = document.querySelector("#about")
 gsap.fromTo(
     about,
     {
-        clipPath: "circle(5% at 50% 8%)",
+        clipPath: "circle(4% at 49.5% 17.5%)",
     },
     {
         clipPath: "circle(75% at 50% 50%)",
@@ -36,31 +36,31 @@ gsap.fromTo(
 
 
 
-let tlProj = gsap.timeline({
-    scrollTrigger:{
-        trigger:"#projects",
-        scrub:1,
-        pin: true,
-        markers: true,
-          snap: {
-      snapTo: 0.33,
-      duration: 0.02,
-      ease: "power1.inOut"
-    },
-    }
-});
+// let tlProj = gsap.timeline({
+//     scrollTrigger:{
+//         trigger:"#projects",
+//         scrub:1,
+//         pin: true,
+//         markers: true,
+//           snap: {
+//       snapTo: 0.33,
+//       duration: 0.02,
+//       ease: "power1.inOut"
+//     },
+//     }
+// });
 
 
-let tlCon = gsap.timeline({
-    scrollTrigger:{
-        trigger:"#contact",
-        scrub:1,
-        pin: true,
-        markers: true,
-          snap: {
-      snapTo: 0.33,
-      duration: 0.02,
-      ease: "power1.inOut"
-    },
-    }
-});
+// let tlCon = gsap.timeline({
+//     scrollTrigger:{
+//         trigger:"#contact",
+//         scrub:1,
+//         pin: true,
+//         markers: true,
+//           snap: {
+//       snapTo: 0.33,
+//       duration: 0.02,
+//       ease: "power1.inOut"
+//     },
+//     }
+// });
--- splineScript.js
+++ splineScript.js
@@ -37,21 +37,21 @@ window.onscroll = function(){
   proj.style.color="white";
   con.style.color="white";
 console.log(top);
-  if(top <= 1250){
-    nav.style.backgroundColor = "rgb(62,17,131)";
+  if(top < 900){
+    nav.style.backgroundColor = "transparent";
     home.style.color="Red";
     about.style.color="white";
     proj.style.color="white";
     con.style.color="white";
   }
-  else if (top > 1250 && top < 2450){
+  else if (top >= 900 && top < 2218){
     nav.style.backgroundColor = "rgb(18,24,85)";
     home.style.color="white";
     about.style.color="red";
     proj.style.color="white";
     con.style.color="white";
   }
-  else if(top >= 2450 && top < 3700 ){
+  else if(top >= 2218 && top < 3700 ){
     nav.style.backgroundColor = "pink";
     home.style.color="white";
     about.style.color="white";
