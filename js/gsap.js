f495e953e65c5f66bfe32b07affd438cec0b7e44
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-03-15 22:29:47 -0500

    adds design for mac os

--- js/gsap.js
+++ js/gsap.js
@@ -78,10 +78,10 @@ var tlproject = gsap.timeline({
   }
 });
 tlproject.fromTo(".circle1", {x: -300, y: -500, autoAlpha: 0}, {x: -400, y: -175, autoAlpha: 1, duration: 1},0)
-         .fromTo(".circle2", {x: 1500, y:-200, autoAlpha: 0}, {x: 800, autoAlpha: 1, duration: 1},0)
+         .fromTo(".circle2", {x: 1500, y:-200, autoAlpha: 0}, {x: 600, autoAlpha: 1, duration: 1},0)
          .fromTo(".circle3", {x: -1500, autoAlpha: 0}, {x: -900, y: 100, autoAlpha: 1, duration: 1},0)
-         .fromTo(".circle4", {x: 2000,y:1000, autoAlpha: 0}, {x:600,y:500, autoAlpha: 1, duration: 1},0)
-         .fromTo(".circle5", {x: -1000,y:2000, autoAlpha: 0}, {x: -500,y:500, autoAlpha: 1, duration: 1},0)
+         .fromTo(".circle4", {x: 2000,y:1000, autoAlpha: 0}, {x:450,y:345, autoAlpha: 1, duration: 1},0)
+         .fromTo(".circle5", {x: -1000,y:2000, autoAlpha: 0}, {x: -500,y:340, autoAlpha: 1, duration: 1},0)
 
          
 //Contact section for 1080 p
@@ -95,8 +95,8 @@ var tlCon = gsap.timeline({
   }
 })
 
-tlCon.fromTo(".mountain1",{x:1000, y:150, autoAlpha: 1}, {x: 0, y: 0, autoAlpha: 1, duration: 1},0)
-     .fromTo(".mountain2",{x:-1500, y:150, autoAlpha: 1}, {x: 0, y: 0, autoAlpha: 1, duration: 1},0)
-     .fromTo(".mountain3",{x:1000, y:150, autoAlpha: 1}, {x: 0, y: 0, autoAlpha: 1, duration: 1},1)
-     .fromTo(".mountain4",{x:0, y:1200, autoAlpha: 1}, {x: 0, y: 0, autoAlpha: 1, duration: 1.5},2)
-     .fromTo(".cardCon",{x:0, y:1000, autoAlpha: 1}, {x: 0, y: 0, autoAlpha: 1, duration: 1.5},3)
+tlCon.fromTo(".mountain1",{x:1000, y:150, autoAlpha: 1}, {x: -370, y: -40, autoAlpha: 1, duration: 1},0)
+     .fromTo(".mountain2",{x:-1500, y:150, autoAlpha: 1}, {x: 275, y: -40, autoAlpha: 1, duration: 1},0)
+     .fromTo(".mountain3",{x:1000, y:150, autoAlpha: 1}, {x: -370, y: -40, autoAlpha: 1, duration: 1},1)
+     .fromTo(".mountain4",{x:0, y:1200, autoAlpha: 1}, {x: 0, y: -40, autoAlpha: 1, duration: 1.5},2)
+     .fromTo(".cardCon",{x:0, y:1000, autoAlpha: 1}, {x: 0, y: -40, autoAlpha: 1, duration: 1.5},3)
